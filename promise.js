edaab7cde3bcc6ed58c6691bd4df7b33f2d185bf
Panu Korpela <panu@vuolearning.fi>
2016-04-29 03:35:34 +0300

    Formatting changes

--- promise.js
+++ promise.js
@@ -1,9 +1,9 @@
-var p = new Promise(function(resolve,reject){
-  setTimeout(function(){
-    resolve("Yay!");
-  },100);
+var p = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('Yay!');
+    }, 100);
 });
 
-p.then(function(msg){
-  console.log(msg); // Yay!
+p.then(function (msg) {
+    console.log(msg);   // Yay!
 });
